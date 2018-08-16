589cc7bc14f371bd64c8c0c13c2242308c81d521
Karina Nowicka <knowicka@gmail.com>
2018-08-16 23:47:28 +0200

    generowanie checkboxów w pętlach

---
+++ js/_filters-feeling-checkboxes.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        txt: 'sobotni wieczór z popcornem',
+        value: 'saturday'
+    },
+    {
+        txt: 'łzy wruszenia',
+        value: 'tears'
+    },
+    {
+        txt: 'dobre polskie kino',
+        value: 'polish'
+    },
+    {
+        txt: 'popraw mi humor',
+        value: 'humor'
+    },
+    {
+        txt: 'zapodaj coś ciężkiego',
+        value: 'serious'
+    }
+];
---
+++ js/_filters-topics-checkboxes.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        txt: 'społeczne',
+        value: 'social'
+    },
+    {
+        txt: 'rodzina, związki, dzieci',
+        value: 'family'
+    },
+    {
+        txt: 'seksualność',
+        value: 'sexuality'
+    },
+    {
+        txt: 'ekologia',
+        value: 'ecology'
+    },
+    {
+        txt: 'odżywianie',
+        value: 'nutrition'
+    },
+    {
+        txt: 'sport',
+        value: 'sport'
+    }
+];
