93809413022852468696399f2b17fd7a29c153f3
Karina Nowicka <knowicka@gmail.com>
2018-08-16 12:15:11 +0200

    sort i clear filters wrzucone do main content header

--- js/_main-content.jsx
+++ js/_main-content.jsx
@@ -3,39 +3,25 @@ import React from 'react';
 import FilmContent from './_film-content.jsx'
 
 
-class ClearFiltersBtn extends React.Component {
-    render() {
-        return (
-            <button className="clear-filters-btn">wyczyść filtry</button>
-        );
-    }
-}
-
-class Sort extends React.Component {
-    render() {
-        return (
-            <div className="sort">
-                <label htmlFor="sort">sortuj wg:
-                    <div className="custom-select">
-                        <select name="sort" id="sort">
-                            <option value="-">-</option>
-                            <option value="az">A-Z</option>
-                            <option value="rate">oceny</option>
-                            <option value="year">roku produkcji</option>
-                        </select>
-                    </div>
-                </label>
-            </div>
-        );
-    }
-}
-
 class MainContentHeader extends React.Component {
     render() {
         return (
             <div className="main-content-header">
-                <Sort/>
-                <ClearFiltersBtn/>
+
+                <div className="sort">
+                    <label htmlFor="sort">sortuj wg:
+                        <div className="custom-select">
+                            <select name="sort" id="sort">
+                                <option value="-">-</option>
+                                <option value="az">A-Z</option>
+                                <option value="rate">oceny</option>
+                                <option value="year">roku produkcji</option>
+                            </select>
+                        </div>
+                    </label>
+                </div>
+
+                <button className="clear-filters-btn">wyczyść filtry</button>
             </div>
         );
     }
