8fde7bf553ad4fcc327f6a90b2433c1b6f23c66c
Karina Nowicka <knowicka@gmail.com>
2018-08-17 16:47:46 +0200

    filtry v0.1

--- js/_filters.jsx
+++ js/_filters.jsx
@@ -4,6 +4,66 @@ import genreArr from './_filters-genre-checkboxes';
 import feelingArr from './_filters-feeling-checkboxes';
 import topicsArr from './_filters-topics-checkboxes';
 
+
+class GenreForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            valuesChecked: [],
+            checked: false
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            valuesChecked: [...this.state.valuesChecked, e.target.value],
+            checked: !this.state.checked
+        });
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        if(this.state.checked === true && typeof this.props.filterFilms === 'function') {
+            this.props.filterFilms(this.state.valuesChecked);
+        }
+
+    };
+
+    render() {
+        console.log(this.state.checked);
+
+        let genreCheckboxes = [];
+        for (let i = 0; i < genreArr.length; i++) {
+            let li = <li key={`genre-${i}`}>
+                <label className="filters-checkbox">
+                    <input type="checkbox"
+                           name="genre"
+                           value={genreArr[i].value}
+                           // checked={this.state.checked}
+                           onChange={e => this.handleChange(e)}/>
+                    <span></span>
+                    {genreArr[i].txt}
+                </label>
+            </li>
+            genreCheckboxes.push(li);
+        }
+
+        return (
+            <div className="filters-section-genre">
+                <h4 className="filters-genre-title">gatunki</h4>
+                <form className="genre-form" onSubmit={e => this.handleSubmit(e)}>
+                    <ul className="genre-list">
+                        {genreCheckboxes}
+                    </ul>
+                    <div className="filters-button-row">
+                        <button type="submit">OK</button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
 class Filters extends React.Component {
     constructor(props) {
         super(props);
@@ -27,13 +87,18 @@ class Filters extends React.Component {
     //
     // };
     //
-    // handleSubmit = (e) => {
-    //     e.preventDefault();
-    //     console.log('cos dziala');
-    // };
 
+    filterFilms = (value) => {
+        console.log(value);
+
+        // this.setState({
+        //     valuesChecked: [...this.state.valuesChecked, value]
+        // });
+    };
 
     render() {
+        console.log(this.state.valuesChecked);
+
         let feelingCheckboxes = [];
         for (let i = 0; i < feelingArr.length; i++) {
             let li = <li key={`feeling-${i}`}>
@@ -64,20 +129,20 @@ class Filters extends React.Component {
             topicsCheckboxes.push(li);
         }
 
-        let genreCheckboxes = [];
-        for (let i = 0; i < genreArr.length; i++) {
-            let li = <li key={`genre-${i}`}>
-                <label className="filters-checkbox">
-                    <input type="checkbox"
-                           name="genre"
-                           value={genreArr[i].value}
-                           checked={this.state.checked}/>
-                    <span></span>
-                    {genreArr[i].txt}
-                </label>
-            </li>
-            genreCheckboxes.push(li);
-        }
+        // let genreCheckboxes = [];
+        // for (let i = 0; i < genreArr.length; i++) {
+        //     let li = <li key={`genre-${i}`}>
+        //         <label className="filters-checkbox">
+        //             <input type="checkbox"
+        //                    name="genre"
+        //                    value={genreArr[i].value}
+        //                    checked={this.state.checked}/>
+        //             <span></span>
+        //             {genreArr[i].txt}
+        //         </label>
+        //     </li>
+        //     genreCheckboxes.push(li);
+        // }
 
         return (
             <div className="filters">
@@ -107,17 +172,7 @@ class Filters extends React.Component {
                     </form>
                 </div>
 
-                <div className="filters-section-genre">
-                    <h4 className="filters-genre-title">gatunki</h4>
-                    <form className="genre-form" onSubmit={e => this.handleSubmit(e)}>
-                        <ul className="genre-list">
-                            {genreCheckboxes}
-                        </ul>
-                        <div className="filters-button-row">
-                            <button type="submit">OK</button>
-                        </div>
-                    </form>
-                </div>
+                <GenreForm filterFilms={this.filterFilms}/>
 
             </div>
         );
